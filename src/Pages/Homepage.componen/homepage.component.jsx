c73f29720e9ff27920ae8e12143fbb55b8f79d27
Diego Navarro <luis_d_100@hotmail.com>
2020-02-17 22:22:20 -0600

    broken components and finish the style

---
+++ src/Pages/Homepage.componen/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+import MenuItem from '../../componets/menu-item/menu-item.component'
+import Directory from '../../componets/directory/directory.component'
+
+
+
+const HomePage = () =>(
+
+    <div className ='homepage'>
+        <Directory />
+    </div>
+)
+
+export default HomePage;
